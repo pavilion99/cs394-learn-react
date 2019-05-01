1c0dc7a54e2677a9d3a53b9bc802542b320abd7a
Spencer Colton <me@scolton.tech>
2019-04-30 22:29:38 -0500

    Make app functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import { withStyles } from '@material-ui/core';
 
@@ -11,42 +11,25 @@ const styles = {
   }
 }
 
-class App extends React.Component {
-  state = {
-    products: []
-  }
+const App = ({ classes }) => {
+  const [products, setProducts] = useState([]);
 
-  componentDidMount(npm) {
-    this.mounted = true;
-
+  useEffect(() => {
     fetch("/products.json").then(result => result.json()).then(res_json => {
-      if (!this.mounted) return;
-
       const { products: products_obj } = res_json;
-      const products = Object.values(products_obj);
-
-      this.setState({
-        products: products
-      });
+      const products_vals = Object.values(products_obj);
+  
+      setProducts(products_vals);
     });
-  }
+  }, []);
 
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  render() {
-    const { products } = this.state;
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.container}>
-        {products.map(product => (
-          <ProductCard key={product.sku} title={product.title} subtitle={product.description} sku={product.sku} price={product.price} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.container}>
+      {products.map(product => (
+        <ProductCard key={product.sku} title={product.title} subtitle={product.description} sku={product.sku} price={product.price} />
+      ))}
+    </div>
+  );
+};
 
 export default withStyles(styles)(App);
